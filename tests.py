cbc03228474210f2388eaf3b7056a9f93612a4fc
Surekha390 <surekha.tallapalli@gmail.com>
2025-01-23 17:36:10 +0100

    Made changes

---
+++ tests.py
@@ -0,0 +1,18 @@
+import unittest
+import calculator 
+# Define a class that inherits from unittest.TestCase
+class TestAddNumbers(unittest.TestCase):
+
+    # Define a test method
+    def test_add_numbers(self):
+        self.assertEqual(calculator.add_numbers(2, 3), 5)
+
+    def test_mul_numbers(self):
+        self.assertEqual(calculator.mul_numbers(5, 3), 15)
+
+    def test_div_numbers(self):
+        self.assertEqual(calculator.div_numbers(10, 5), 2)
+
+    def subtraction(self):
+        self.asserEqual(calculator.subtraction(8, 2), 6)
+
